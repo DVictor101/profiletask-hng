e7a33ff0c86e5e75f009e3174776b38d313be7f2
Segun(RenewedCrown) <seguntimileyin489@gmail.com>
2023-09-07 14:33:48 +0100

    adding the profile page code

---
+++ script.js
@@ -0,0 +1,31 @@
+const currentDay = document.querySelector(
+ ".currentDayOfTheWeek"
+);
+
+const currentUtcTime = document.querySelector(
+ ".currentUTCTime"
+);
+//GETTING THE CURRENT DAY CODE
+const currentDate = new Date();
+
+const daysOfWeek = [
+ "Sunday",
+ "Monday",
+ "Tuesday",
+ "Wednesday",
+ "Thursday",
+ "Friday",
+ "Saturday",
+];
+
+const dayOfWeekIndex = currentDate.getDay();
+const currentDayOfWeek =
+ daysOfWeek[dayOfWeekIndex];
+currentDay.textContent = currentDayOfWeek;
+
+//CODE FOR GETTING THE CURRENT UTC TIME
+// Get the current UTC time in milliseconds
+const currentUTCTime = new Date().getTime();
+
+// Display the current UTC time
+currentUtcTime.textContent = currentUTCTime;
